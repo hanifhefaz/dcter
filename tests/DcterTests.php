a76ab30af83597a94620da5995b67f283527b261
hanifhefaz <=>
2023-04-14 15:29:35 +0430

    update tests according to changes

--- tests/DcterTests.php
+++ tests/DcterTests.php
@@ -21,14 +21,14 @@ class DcterTests extends TestCase
 
     public function testGregorianToJalali()
     {
-        $expected = "1402-1-24";
+        $expected = "1402-01-24";
         $actual = Dcter::GregorianToJalali("2023-04-13", "YYYY-MM-DD");
         $this->assertEquals($expected, $actual);
     }
     
     public function testJalaliToGregorian()
     {
-        $expected = "2023-4-13";
+        $expected = "2023-04-13";
         $actual = Dcter::JalaliToGregorian("1402-01-24", "YYYY-MM-DD");
         $this->assertEquals($expected, $actual);
     }
